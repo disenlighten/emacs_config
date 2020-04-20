7677019becd1dbb33abf3b8d4dae6aae55398e84
James Pemantell <james@Jamess-MacBook-Pro.local>
2020-04-19 22:03:10 -0400

    Initial commit of mytags file. Added configs for org mode shortcuts and org-agenda includes

---
+++ mytags.el
@@ -0,0 +1,9 @@
+(provide 'mytags)
+
+(setq org-tag-alist '(("@elisp" . ?l) ("@python" . ?p) ("@emacs" . ?e) ("@wireless" . ?w) ("@linux" . ?L)))
+
+;; (custom-set-variables
+;;  '(org-directory "/Users/james/repos/jamienotes")
+;; '(org-agenda-files (list org-directory)))
+
+(setq org-agenda-files '("/Users/james/repos/jamienotes/daily"))
